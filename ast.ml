18c1ecee4ac5a51eacfd64180ba435bb6daffc56
Elias Razi <Kontakt.eliasrazi@gmail.com>
2026-03-10 10:34:31 +0100

    test run

--- ast.ml
+++ ast.ml
@@ -1,30 +1,39 @@
 (* Abstract Syntax of Markdowns *)
+
+type location = Lexing.position * Lexing.position
+type ident = { loc: location; id: string }
+
 type abbreviation = string
 
 type constant =
-  | Cn of float
-  | Cb of bool
-  | Cs of string
-  | Ca of abbreviation
+  | Cfloat of float
+  | Cbool of bool
+  | Cstring of string
+  | Cabbr of abbreviation
+
+type unop =
+  | Unot
 
 type binop =
-  | Badd | Bsub | Bmul | Bdiv | Bmod    (* + - * // % *)
+  | Badd | Bsub | Bmul | Bdiv | Bmod    (* + - * / % *)
   | Beq | Bneq | Blt | Ble | Bgt | Bge  (* == != < <= > >= *)
-  | Band | Bor                          (* and or *)
+  | Band | Bor                           (* and or *)
 
 type expr =
   | Ec of constant
-  | Eid of string
-  | Ebinop of binop * expr * expr       (* binary operation *)
+  | Eident of ident
+  | Ebinop of binop * expr * expr        (* binary operation *)
+  | Eunop of unop * expr
 
 type stmt =
-  | Sassign of string * expr            (* assignment *)
-  | Sseq of stmt * stmt                 (* sequence of statements *)
-  | Sif of expr * stmt * stmt           (* if-then-else *)
+  | Sassign of ident * expr              (* assignment *)
+  | Sseq of stmt * stmt                  (* sequence of statements *)
+  | Sif of expr * stmt * stmt            (* if-then-else *)
+  | Snop                                 (* empty branch *)
 
 type doc_element =
   | RawText of string
-  | Annotation of string
-  | Script of stmt                      (* The §§ or §/ /§ blocks *)
+  | Annotation of expr                   
+  | Script of stmt                       (* The §§ or §/ /§ blocks *)
 
 type document = doc_element list
